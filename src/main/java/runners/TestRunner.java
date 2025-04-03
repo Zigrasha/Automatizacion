d699e71bf04ed990636375908cc9358a04a52f17
Edgardo Fernandez <174687686+edfernandeza@users.noreply.github.com>
2025-04-03 17:25:35 -0500

    Fix: Uso de hooks en runner

--- src/main/java/runners/TestRunner.java
+++ src/main/java/runners/TestRunner.java
@@ -7,9 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features", // Ruta de los archivos .feature
-        glue = {"stepDefinitions"}, // Paquete donde se encuentran los Step Definitions
+        glue = {"stepDefinitions", "hooks"}, // Paquete donde se encuentran los Step Definitions
         plugin = {"pretty", "html:target/cucumber-reports"}, // Plugins de reportes
-        tags = "@TituloBorrado"
+        tags = "@Login"
 )
 public class TestRunner {
     // Configuración para ejecutar los escenarios de prueba definidos en los archivos .feature
